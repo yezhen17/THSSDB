2875ed4680e58a4644547e5c1e203b5f5216e6a7
Yezhen Cong <52420115+THU17cyz@users.noreply.github.com>
2020-05-01 19:57:13 +0800

    de&serialization

---
+++ src/main/java/cn/edu/thssdb/exception/SerializeException.java
@@ -0,0 +1,12 @@
+package cn.edu.thssdb.exception;
+
+import java.io.IOException;
+
+
+// ignore
+public class SerializeException extends IOException {
+    @Override
+    public String getMessage() {
+        return "Exception: File not found during serialization!";
+    }
+}
--- src/main/java/cn/edu/thssdb/schema/Table.java
+++ src/main/java/cn/edu/thssdb/schema/Table.java
@@ -1,8 +1,10 @@
 package cn.edu.thssdb.schema;
 
+import cn.edu.thssdb.exception.SerializeException;
 import cn.edu.thssdb.index.BPlusTree;
 import javafx.util.Pair;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -35,13 +37,34 @@ public class Table implements Iterable<Row> {
     // TODO
   }
 
-  private void serialize() {
+  private void serialize() throws IOException {
     // TODO
+    // 序列化和反序列化的基础用法，不过怎么和索引结合还是个问题
+    // 可参考 https://blog.csdn.net/u014240769/article/details/84903409
+    ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("temp.txt"));
+    Iterator<Row> row_iter = iterator();
+    while (row_iter.hasNext()) {
+      objectOutputStream.writeObject(row_iter.next());
+    }
+    objectOutputStream.flush();
+    objectOutputStream.close();
   }
 
-  private ArrayList<Row> deserialize() {
+  private ArrayList<Row> deserialize() throws IOException, ClassNotFoundException {
     // TODO
-    return null;
+    ArrayList<Row> rows = new ArrayList<>();
+    ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("temp.txt"));
+    while (true) {
+      Row row = (Row) objectInputStream.readObject();
+      if (row != null) {
+        rows.add(row);
+      } else {
+        break;
+      }
+    }
+    objectInputStream.close();
+    return rows;
+    // return null;
   }
 
   private class TableIterator implements Iterator<Row> {
