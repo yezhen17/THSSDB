b86f39522bc0d48585a8d3d052cfa69e84dfde12
Yezhen Cong <52420115+THU17cyz@users.noreply.github.com>
2020-05-02 17:16:07 +0800

    fixed a conflict

---
+++ ThssDB/src/main/java/cn/edu/thssdb/schema/Table.java
@@ -0,0 +1,133 @@
+package cn.edu.thssdb.schema;
+
+import cn.edu.thssdb.exception.SerializeException;
+import cn.edu.thssdb.index.BPlusTree;
+import javafx.util.Pair;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/***************
+ * [class] 表
+ ***************/
+public class Table implements Iterable<Row> {
+  ReentrantReadWriteLock lock;            // 可重入读写锁
+  private String databaseName;            // 数据库名称
+  public String tableName;                // 表名称
+  public ArrayList<Column> columns;       // 列定义表
+  public BPlusTree<Entry, Row> index;     // B+树索引
+  private int primaryIndex;               // 主键索引
+  private PersistentStorage<Row> persistentStorage;
+
+  /**
+   * [method] 构造方法
+   * @param databaseName {String} 数据库名称
+   * @param tableName {String} 表名称
+   * @param columns {Column[]} 列定义表
+   * @param primaryIndex {int} 主键索引
+   */
+  public Table(String databaseName, String tableName, Column[] columns, int primaryIndex) {
+    this.databaseName = databaseName;
+    this.tableName = tableName;
+    this.columns = new ArrayList<>(Arrays.asList(columns));
+    this.primaryIndex = primaryIndex;
+    this.persistentStorage = new PersistentStorage<>("tmp.data"); // this is the storage file name
+  }
+
+  /**
+   * [method] 恢复表
+   * [note] 从持久化数据中恢复表
+   * @exception TODO
+   */
+  private void recover() {
+    // TODO
+  }
+
+  /**
+   * [method] 插入行
+   * @param row {Row} 待插入行
+   * @exception TODO
+   */
+  public void insert(Row row) {
+    // TODO
+  }
+
+  /**
+   * [method] 删除行
+   * TODO 可能利用索引
+   * @exception TODO
+   */
+  public void delete() {
+    // TODO
+  }
+
+  /**
+   * [method] 更新行
+   * TODO 可能利用索引
+   * @exception TODO
+   */
+  public void update() {
+    // TODO
+  }
+
+  /**
+   * [method] 序列化
+   */
+  private void serialize() throws IOException {
+    // TODO
+    try {
+      lock.readLock().lock();
+      persistentStorage.serialize(iterator());
+    } finally {
+      lock.readLock().unlock();
+    }
+  }
+
+  /**
+   * [method] 反序列化
+   */
+  private ArrayList<Row> deserialize() throws IOException, ClassNotFoundException {
+    // TODO
+    return persistentStorage.deserialize();
+  }
+
+  /**
+   * [class] 表迭代器（按行）
+   */
+  private class TableIterator implements Iterator<Row> {
+    private Iterator<Pair<Entry, Row>> iterator;
+
+    TableIterator(Table table) {
+      this.iterator = table.index.iterator();
+    }
+
+    @Override
+    public boolean hasNext() {
+      return iterator.hasNext();
+    }
+
+    @Override
+    public Row next() {
+      return iterator.next().getValue();
+    }
+  }
+
+  /**
+   * [method] 创建迭代器（按行）
+   */
+  @Override
+  public Iterator<Row> iterator() {
+    return new TableIterator(this);
+  }
+
+  /**
+   * [method] 检查列定义
+   */
+  public static boolean checkColumns(Column[] columns, int primaryIndex) {
+    // TODO 可整合至他处
+    return true;
+  }
+}
